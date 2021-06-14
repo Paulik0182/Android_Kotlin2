dd821d7eacbca441c24e099cb245d85666eb4654
Paveł <Paulik0182@gmail.com>
2021-06-14 13:18:10 +0300

    лекция

--- app/src/main/java/com/android/kotlin/framework/MainActivity.kt
+++ app/src/main/java/com/android/kotlin/framework/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.kotlin
+package com.android.kotlin.framework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.kotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/android/kotlin/id/KoinModule.kt
@@ -0,0 +1,9 @@
+package com.android.kotlin.id
+
+import org.koin.dsl.bing
+import org.koin.dsl.module
+
+var appModule = module {
+
+
+}
